ba6ee9ea097d9d3ac4b229af6e7499d47c811df7
Yoko Yamaguchi <yamag104@mail.chapman.edu>
2015-08-18 22:20:47 -0700

    Assignment 8 completed

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -12,17 +12,19 @@
 
 - (NSInteger) increaseNumberBy1:(NSInteger) number {
     /* WORK HERE */
+    number++;
     return number;
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
     /* WORK HERE */
+    number1+=number2;
     return number1;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
     /* WORK HERE */
-    return 0;
+    return (dividend%divisor);
 }
 
 @end
