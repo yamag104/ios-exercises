d7d694bc9b11b2482bb986ca2b25fc229b88be6d
Yoko Yamaguchi <yamag104@mail.chapman.edu>
2015-08-22 14:40:29 -0700

    Checkpoint 15 Completed

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,18 +11,37 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    NSNumber *doubledNumber = @([number integerValue] *2);
+    return doubledNumber;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+    NSInteger higherNumber, lowerNumber;
+    if (otherNumber < number) {
+        lowerNumber = otherNumber;
+        higherNumber = number;
+    }
+    else {
+        lowerNumber = number;
+        higherNumber = otherNumber;
+    }
+    NSInteger length = higherNumber - lowerNumber + 1;
+    NSMutableArray *mutableArr = [[NSMutableArray alloc] init];
+    for (NSInteger i=0; i<length; ++i) {
+        [mutableArr insertObject:[NSNumber numberWithInteger:lowerNumber++] atIndex:i];
+    }
+    NSArray *array = [mutableArr copy];
+    return array;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+    NSInteger min = 0;
+    for (NSInteger i=0; i<[arrayOfNumbers count]; ++i) {
+        if ([arrayOfNumbers[i] intValue] < min) {
+            min = [arrayOfNumbers[i] intValue];
+        }
+    }
+    return min;
 }
 
 @end
